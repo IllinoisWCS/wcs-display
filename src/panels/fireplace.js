e28d7a04ebb41952e1972729193052eaf14586e9
Kevin Wang <kevin@kevinwang.com>
2016-12-07 13:02:20 -0600

    Fix video looping and remove controls and info.

--- src/panels/fireplace.js
+++ src/panels/fireplace.js
@@ -16,7 +16,7 @@ var FireplacePanel = React.createClass({
                 <iframe 
                     width="560"
                     height="315" 
-                    src="https://www.youtube.com/embed/0fYL_qiDYf0?loop=1&autoplay=1"
+                    src="https://www.youtube.com/embed/0fYL_qiDYf0?playlist=0fYL_qiDYf0&loop=1&autoplay=1&iv_load_policy=3&controls=0&showinfo=0"
                     frameBorder="0"
                     className="video">
                 </iframe>
